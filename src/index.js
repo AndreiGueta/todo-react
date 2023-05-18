9758f818ef41cacbe6feac44bf35dffbd02e525b
Andrei Paulo T. Gueta <andrei.gueta@guerilla360.com>
2023-05-18 14:02:17 +0800

    Messed up with monday

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App';
-import '@fontsource/poppins';
-import '@fontsource/poppins/500.css';
-import '@fontsource/poppins/600.css';
-import '@fontsource/poppins/700.css';
-import './styles/GlobalStyles.css';
-import { store } from './app/store';
+import React from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
